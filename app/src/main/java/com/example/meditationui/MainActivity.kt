962461cf5191d776e916069a17e9a691551af885
Zidan <zidanali111@gmail.com>
2023-11-17 20:35:04 +0530

    final Commit.

--- app/src/main/java/com/example/meditationui/MainActivity.kt
+++ app/src/main/java/com/example/meditationui/MainActivity.kt
@@ -1,19 +1,18 @@
-package com.example.meditationui
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-
+import com.example.meditationui.ui.HomeScreen
+import com.example.meditationui.ui.theme.MeditationUITheme
 
 class MainActivity : ComponentActivity() {
     @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MeditationUITheme{
+                HomeScreen()
             }
         }
     }
+}
